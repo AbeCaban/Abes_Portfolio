e9ce39bb2ee82ff5bd7cfe93e893957899d29997
Abe <111446302+AbeCaban@users.noreply.github.com>
2025-01-23 23:07:53 -0500

    code basics mana update 1/23/2025

--- Python/Code_basics.py
+++ Python/Code_basics.py
@@ -1,3 +1,7 @@
+# making a code that takes mana away every time 
+# the user hits a key, then returns the mana lost over time. 
+# everytime mana is lost it will print the amount of mana lost and the list of the mana available.
+
 mana_available = ['blue10', 'blue20', 'blue30', 'blue40', 'blue50', 'blue60', 'blue70', 'blue80', 'blue90', 'blue100']
 
 mana_lost = []
