76983695a9140ecd88f10da5de26487113588a1d
Abe <111446302+AbeCaban@users.noreply.github.com>
2025-02-24 14:00:13 -0500

    Code Signal Practice 2/24/2025

--- Python/Code_Signal_practice.py
+++ Python/Code_Signal_practice.py
@@ -36,34 +36,61 @@
 # print(intersecting_elements({1, 3, 5, 7, 9}, {2, 4, 6, 8, 10}))
 # # Expected output: []
 
-def exclusive_products(inventory1, inventory2):
+# def exclusive_products(inventory1, inventory2):
+#     # implement this
+#     inventory1 = set(inventory1)
+#     inventory2 = set(inventory2)
+#     inventory1_upper = {item.upper() for item in inventory1}
+#     inventory2_upper = {item.upper() for item in inventory2}
+    
+#     common_item = inventory1_upper & inventory2_upper
+#     inventory1_upper -= common_item
+#     inventory2_upper -= common_item
+    
+#     inventory1_upper = list(inventory1_upper)
+#     inventory2_upper = list(inventory2_upper)
+    
+#     return inventory1_upper, inventory2_upper
+    
+
+# inventory1 = ["Shirt", "Jeans", "Hat"]
+# inventory2 = ["jeans", "Belt", "Boots"]
+# print(exclusive_products(inventory1, inventory2))
+# # Expected output: (['HAT', 'SHIRT'], ['BELT', 'BOOTS'])
+
+# inventory1 = ["T-Shirt", "hoodie", "Backpack"]
+# inventory2 = ["Backpack", "Hoodie", "t-shirt"]
+# print(exclusive_products(inventory1, inventory2))
+# # Expected output: ([], [])
+
+# inventory1 = []
+# inventory2 = ["Dress", "Skirt", "Coat"]
+# print(exclusive_products(inventory1, inventory2))
+# # Expected output: ([], ['COAT', 'DRESS', 'SKIRT'])
+
+def find_unique_string(words):
     # implement this
-    inventory1 = set(inventory1)
-    inventory2 = set(inventory2)
-    inventory1_upper = {item.upper() for item in inventory1}
-    inventory2_upper = {item.upper() for item in inventory2}
     
-    common_item = inventory1_upper & inventory2_upper
-    inventory1_upper -= common_item
-    inventory2_upper -= common_item
+    single_words = set()
+    repeated_words = set()
+    for word in words:
+        if word in single_words:
+            repeated_words.add(word)
+        else:
+            single_words.add(word)
+    unique_words_in_set = single_words - repeated_words
     
-    inventory1_upper = list(inventory1_upper)
-    inventory2_upper = list(inventory2_upper)
+    unique_words_in_list = list(unique_words_in_set)
     
-    return inventory1_upper, inventory2_upper
+    if not unique_words_in_list:
+        unique_words_in_list.append('')
+    
+    final_result = ", ".join([f"'{item}'" for item in unique_words_in_list])
+            
+    return final_result
     
 
-inventory1 = ["Shirt", "Jeans", "Hat"]
-inventory2 = ["jeans", "Belt", "Boots"]
-print(exclusive_products(inventory1, inventory2))
-# Expected output: (['HAT', 'SHIRT'], ['BELT', 'BOOTS'])
-
-inventory1 = ["T-Shirt", "hoodie", "Backpack"]
-inventory2 = ["Backpack", "Hoodie", "t-shirt"]
-print(exclusive_products(inventory1, inventory2))
-# Expected output: ([], [])
-
-inventory1 = []
-inventory2 = ["Dress", "Skirt", "Coat"]
-print(exclusive_products(inventory1, inventory2))
-# Expected output: ([], ['COAT', 'DRESS', 'SKIRT'])
+print(find_unique_string(['apple', 'banana', 'apple', 'mango', 'banana']))  # It should print: 'mango' 'pineapple
+print(find_unique_string(['hello', 'world', 'hello']))  # It should print: 'world' 'pineapple
+print(find_unique_string(['hello', 'world', 'hello', 'world']))  # It should print: ''
+print(find_unique_string([]))  # It should print: ''
