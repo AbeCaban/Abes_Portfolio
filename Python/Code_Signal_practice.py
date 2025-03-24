1e8f0b5075d47f93df01bd724a8e716b11fc1346
Abe <111446302+AbeCaban@users.noreply.github.com>
2025-03-24 19:57:29 -0400

    Code Signal Practice 3/24/2025

--- Python/Code_Signal_practice.py
+++ Python/Code_Signal_practice.py
@@ -248,3 +248,16 @@
 #         print(" ", museum)
 # -----------------------------------------------------------------------------------
 
+def calculate_trip_cost(countries, country_costs):
+    total_cost = 0
+    for country in countries:
+        total_cost += country_costs[country]
+    return total_cost
+
+# Presuming the chosen_countries and their costs
+chosen_countries = ['France', 'Italy', 'Spain']
+country_costs = {'France': 1200, 'Italy': 950, 'Spain': 800}
+
+# Call the function
+trip_cost = calculate_trip_cost(chosen_countries, country_costs)
+print(f"The total cost of the trip is: ${trip_cost}")
