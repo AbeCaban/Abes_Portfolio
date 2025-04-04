64d7a6b44254ac5058dc3bbaf396e04e85ac8807
Abe <111446302+AbeCaban@users.noreply.github.com>
2025-04-04 12:38:19 -0400

    Code Signal Practice 4/4/2025

--- Python/Code_Signal_practice.py
+++ Python/Code_Signal_practice.py
@@ -283,28 +283,39 @@
 
 # -----------------------------------------------------------------------------------
 # Define multiple functions and use them together to solve a problem.
-def choose_countries(budget, costs):
-    total_cost = 0
-    chosen_countries = []
-    for country, cost in costs.items():
-        if total_cost + cost > budget:
-            break
-        total_cost += cost
-        chosen_countries.append(country)
-    return chosen_countries
+# def choose_countries(budget, costs):
+#     total_cost = 0
+#     chosen_countries = []
+#     for country, cost in costs.items():
+#         if total_cost + cost > budget:
+#             break
+#         total_cost += cost
+#         chosen_countries.append(country)
+#     return chosen_countries
 
-def calculate_cost(countries, costs):
-    total_cost = 0
-    for country in countries:
-        total_cost += costs[country]
-    return total_cost
+# def calculate_cost(countries, costs):
+#     total_cost = 0
+#     for country in countries:
+#         total_cost += costs[country]
+#     return total_cost
 
-# Assuming sample data for budget and costs for demonstration
-travel_budget = 5000
-country_costs = {'France': 1200, 'Italy': 1500, 'Spain': 800, 'Germany': 900, 'Greece': 1100}
+# # Assuming sample data for budget and costs for demonstration
+# travel_budget = 5000
+# country_costs = {'France': 1200, 'Italy': 1500, 'Spain': 800, 'Germany': 900, 'Greece': 1100}
 
-chosen_countries = choose_countries(travel_budget, country_costs)
-trip_cost = calculate_cost(chosen_countries, country_costs)
+# chosen_countries = choose_countries(travel_budget, country_costs)
+# trip_cost = calculate_cost(chosen_countries, country_costs)
 
-print(f"The countries included in the trip are: {chosen_countries}")
-print(f"The total cost of the trip is: ${trip_cost}")
+# print(f"The countries included in the trip are: {chosen_countries}")
+# print(f"The total cost of the trip is: ${trip_cost}")
+
+# -----------------------------------------------------------------------------------
+# Our list of destinations
+travel_destinations = ["Paris", "Sydney", "Tokyo", "New York", "London"]
+
+# Todo: Insert "Berlin" at the third position using the insert() function
+travel_destinations.insert(4, "Berlin")
+travel_destinations.remove("New York")
+
+# Checking the list
+print(travel_destinations)
